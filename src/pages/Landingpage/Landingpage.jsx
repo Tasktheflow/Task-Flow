1a9ed2b72fc70325ba4ecff468caf8f0b6dcc70b
oyin16 <olukunlehameedat@yahoo.com>
2025-12-16 16:27:22 +0100

    Nav bar completed

--- src/pages/Landingpage/Landingpage.jsx
+++ src/pages/Landingpage/Landingpage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 const landingpage = () => {
   return (
-    <div>landingpage</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
