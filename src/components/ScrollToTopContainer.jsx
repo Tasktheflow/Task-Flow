26bb4ad9b1498d4cc1bb553e844deeb7d725a102
Frostdevs <adesanyanifemi1@gmail.com>
2025-12-22 15:17:58 -0700

    routed pages

--- src/components/ScrollToTopContainer.jsx
+++ src/components/ScrollToTopContainer.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router";
 
 const ScrollToTopContainer = () => {
   const { pathname } = useLocation();
@@ -14,6 +14,7 @@ const ScrollToTopContainer = () => {
       });
     }
   }, [pathname]);
+
 
   return null;
 };
